5ffc40f7fac8e389f20ac7afef1ca22dbe326d1a
Darcy70 <darcy0721@gmail.com>
2017-05-15 20:02:16 +0800

    getting the Admin authentication part done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def is_admin?
+    if !current_user.is_admin?
+      flash[:alert] = "Sorry, You are not an Admin "
+      redirect_to root_path
+    end
+  end
+
+  
 end
