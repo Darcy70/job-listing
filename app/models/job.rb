e28f13c22d81818b535bc7cd0651821561c15547
Darcy70 <darcy0721@gmail.com>
2017-05-16 11:04:40 +0800

    Something wrong with the colomn in the new job page.

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,4 +14,20 @@
 #
 
 class Job < ApplicationRecord
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
+  validates :title, presence: { message: "You need to provide a job title."},
+  length: 3..15
+  validates :description, presence: {:message => "Description is needed"}
+  validates :contact_email, presence: {:message => "Email is needed"},
+  :format => {:with => EMAIL_REGEX, message: "The email format has to be right"}
+
+  validates_numericality_of :wage_lower_bound, :wage_upper_bound
+
+  validates :wage_lower_bound, presence: { message: "You need to provide a lower bound"}, numericality:{ greater_than: 0, message: "It has to be above the zero",less_than: :wage_upper_bound, message: "It has to be below the upper bound"}
+  validates :wage_upper_bound, presence: { message: "You need to provide a upper bound"}, numericality:{ greater_than: :wage_lower_bound, message: "It has to be above the lower bound"}
+
+
+
 end
