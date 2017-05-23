b607ad925e0a98d47b5168f21b4fc9d8dfb402cd
Darcy70 <darcy0721@gmail.com>
2017-05-23 21:26:45 +0800

    add categories and location to jobs in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,20 +12,20 @@ puts "This seed file will automatically create one admin account, 10 jobs been p
 create_account = User.create([email: 'example@gmail.com', password: '12345678', is_admin: true, is_website_admin: true, name: "Powerful User"])
 puts "Admin account has been created "
 
-create_locations = Location.create([{ name: 'Beijing', sort: 1 }, { name: 'Shanghai', sort: 2 }, { name: 'New York', sort: 3 }, { name: 'Tokyo', sort: 4 }, { name: 'Los Angeles', sort: 5 } ])
+create_locations = Location.create!([{ name: 'Beijing', sort: 1 }, { name: 'Shanghai', sort: 2 }, { name: 'New York', sort: 3 }, { name: 'Tokyo', sort: 4 }, { name: 'Los Angeles', sort: 5 } ])
 puts "5 locations created."
 
 
-create_categorys = Category.create([{ name: 'Technology', icon: 'fa fa-laptop' , sort: 1, is_lock: true }, { name: 'Product', icon: 'fa fa-tasks' , sort: 2, is_lock: true }, { name: 'Design', icon: 'fa fa-photo' , sort: 3, is_lock: true }])
+create_categorys = Category.create!([{ name: 'Technology', icon: 'fa fa-laptop' , sort: 1, is_lock: true }, { name: 'Product', icon: 'fa fa-tasks' , sort: 2, is_lock: true }, { name: 'Design', icon: 'fa fa-photo' , sort: 3, is_lock: true },{ name: 'Marketing', icon: 'fa fa-pie-chart' , sort: 5, is_lock: true }, { name: 'Sales', icon: 'fa fa-cny' , sort: 6, is_lock: true }, { name: 'office', icon: 'fa fa-paste' , sort: 7, is_lock: true }])
 puts "3 categories created."
 
 create_jobs = for i in 1..10 do
-  Job.create!([title: "Job No.#{i}", description: " This is the No.#{i} public job been created", contact_email: "JobCreatedBySeedNumber#{i}@example.com",  wage_lower_bound: rand(10..49)*100, wage_upper_bound: rand(50..99)*100, is_hidden: false, company: "No.#{i} company!" ])
+  Job.create!([title: "Job No.#{i}", description: " This is the No.#{i} public job been created", contact_email: "JobCreatedBySeedNumber#{i}@example.com",  wage_lower_bound: rand(10..49)*100, wage_upper_bound: rand(50..99)*100, is_hidden: false, company: "No.#{i} company!", category_id: rand(1..7), location_id: rand(1..5) ])
 end
 puts "10 Public jobs have been created. "
 
 
 create_jobs = for i in 1..10 do
-  Job.create!([title: "Job No.#{i}", description: " This is the No.#{i} hidden job been created ", contact_email: "JobCreatedBySeedNumber#{i}@example.com", wage_lower_bound: rand(10..49)*100, wage_upper_bound: rand(50..99)*100, is_hidden: true, company: "No.#{i} company!"])
+  Job.create!([title: "Job No.#{i}", description: " This is the No.#{i} hidden job been created ", contact_email: "JobCreatedBySeedNumber#{i}@example.com", wage_lower_bound: rand(10..49)*100, wage_upper_bound: rand(50..99)*100, is_hidden: true, company: "No.#{i} company!",category_id: rand(1..7), location_id: rand(1..5)])
 end
 puts "10 Hidden jos have been created. "
