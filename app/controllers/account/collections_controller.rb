d40a6d3cc2edc30cd9df367ced2e985eaf4b713e
Darcy70 <darcy0721@gmail.com>
2017-05-24 19:48:47 +0800

    add collection part and generate the devise views

--- app/controllers/account/collections_controller.rb
+++ app/controllers/account/collections_controller.rb
@@ -1,5 +1,9 @@
 class Account::CollectionsController < ApplicationController
+
   def index
-    
+    @user = current_user
+    @collections = Collection.where(:user => @user).newest_first.paginate(:page => params[:page], per_page: 7)
   end
+
+
 end
